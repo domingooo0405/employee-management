03312d389752fdaa0547cda9c777923237d0ade5
domingooo0405 <domingomacunayjr@gmail.com>
2022-02-22 10:45:46 +0800

    with register

---
+++ grocery-bill-app-frontend-auth-register/src/components/AddItemComponent.jsx
@@ -0,0 +1,212 @@
+import React, { Component } from "react";
+
+
+import grocery from "../images/grocery-logo.jpg";
+import ItemService from "../api/ItemService";
+
+class AddItemComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      id: this.props.match.params.id,
+      name: "",
+      originalPrice: "",
+      discountPercentage: "",
+    };
+
+    this.changeName = this.changeName.bind(this);
+    this.changeOriginalPrice = this.changeOriginalPrice.bind(this);
+    this.changeDiscountPercentage = this.changeDiscountPercentage.bind(this);
+
+    this.addOrUpdate = this.addOrUpdate.bind(this);
+
+    this.cancel = this.cancel.bind(this);
+  }
+  changeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+  changeOriginalPrice = (event) => {
+    this.setState({
+      originalPrice: event.target.value,
+    });
+  };
+  changeDiscountPercentage = (event) => {
+    this.setState({
+      discountPercentage: event.target.value,
+    });
+  };
+
+  componentDidMount() {
+    if (this.state.id === "_add") {
+      return;
+    } else {
+      ItemService.getItemById(this.state.id).then((response) => {
+        let item = response.data;
+        this.setState({
+          name: item.name,
+          originalPrice: item.originalPrice,
+          discountPercentage: item.discountPercentage,
+        });
+      });
+    }
+  }
+
+  addOrUpdate = (event) => {
+    event.preventDefault();
+
+    let item = {
+      name: this.state.name,
+      originalPrice: this.state.originalPrice,
+      discountPercentage: this.state.discountPercentage,
+    };
+    const Name = document.querySelector("#pName");
+    const OPrice = document.querySelector("#oPrice");
+    const dPercent = document.querySelector("#dPercentage");
+    const Message = document.querySelector("#msg");
+
+    if (
+      Name.value.length === 0 ||
+      OPrice.value.length === 0 ||
+      dPercent.value.length === 0
+    ) {
+      Message.innerHTML = "<h4 class='error'> Please complete details!<h4/>";
+
+      setTimeout(() => document.querySelector(".error").remove(), 3000);
+    } else {
+      if (this.state.id === "_add") {
+        let DiscountPercentage = document.getElementById("dPercentage").value;
+        let Price = document.getElementById("oPrice").value;
+        if (DiscountPercentage > 100) {
+          Message.innerHTML =
+            "<h4 class='error'>Not more than 100% discount!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else if (DiscountPercentage < 0) {
+          Message.innerHTML =
+            "<h4 class='error'>Not less than 0% discount!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else if (Price <= 0) {
+          Message.innerHTML =
+            "<h4 class='error'>Not less than or equal 0 price!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else {
+          alert("Successfully Added!");
+
+          ItemService.insertItem(item).then((response) => {
+            this.props.history.push("/items");
+          });
+        }
+      } else {
+        let DiscountPercentage = document.getElementById("dPercentage").value;
+        let Price = document.getElementById("oPrice").value;
+        if (
+          Name.value.length === 0 ||
+          OPrice.value.length === 0 ||
+          dPercent.value.length === 0
+        ) {
+          Message.innerHTML =
+            "<h4 class='error'> Please complete details!<h4/>";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else if (Price <= 0) {
+          Message.innerHTML =
+            "<h4 class='error'>Not less than or equal 0 price!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else if (DiscountPercentage > 100) {
+          Message.innerHTML =
+            "<h4 class='error'>Not more than 100% discount!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else if (DiscountPercentage < 0) {
+          Message.innerHTML =
+            "<h4 class='error'>Not less than 0% discount!<h4/> ";
+
+          setTimeout(() => document.querySelector(".error").remove(), 3000);
+        } else {
+          alert("Successfully Updated!");
+          ItemService.updateItem(item, this.state.id).then((response) => {
+            this.props.history.push("/items");
+          });
+        }
+      }
+    }
+  };
+
+  cancel() {
+    this.props.history.push("/items");
+  }
+  getTitle() {
+    if (this.state.id === "_add") {
+      return <h3> Add Item</h3>;
+    } else {
+      return <h3> Update Item</h3>;
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="wrapper">
+          <form className="login">
+            <p className="title">{this.getTitle()}</p>
+            <div id="msg"></div>
+            <img src={grocery} className="logo-grocery-add" alt=""></img>
+            <input
+              id="pName"
+              placeholder="Product Name"
+              className="form-control"
+              value={this.state.name}
+              onChange={this.changeName}
+              autofocus
+              required
+              
+            />
+            <i className="fa fa-shopping-cart" />
+            <input
+              id="oPrice"
+              placeholder="Price"
+              className="form-control"
+              value={this.state.originalPrice}
+              onChange={this.changeOriginalPrice}
+              type="number"
+              required
+            />
+            <i className="fa fa-dollar" />
+            <input
+              id="dPercentage"
+              placeholder="Discount Percentage"
+              className="form-control"
+              value={this.state.discountPercentage}
+              onChange={this.changeDiscountPercentage}
+              type="number"
+              required
+            />
+            <i className="fa fa-percent" />
+            <br></br>
+            <button
+              style={{ marginLeft: "70px" }}
+              className="button-save"
+              onClick={this.addOrUpdate}
+            >
+              Save
+            </button>
+            <button
+              style={{ marginLeft: "20px" }}
+              className="button-cancel"
+              onClick={this.cancel}
+            >
+              Cancel
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddItemComponent;
---
+++ grocery-bill-app-frontend-auth-register/src/pages/LoginComponent.jsx
@@ -0,0 +1,145 @@
+import react, { useState } from "react";
+import { connect } from "react-redux";
+import { authenticate, authFailure, authSuccess } from "../redux/authActions";
+
+import { userLogin } from "../api/AuthenticationService";
+import { Alert, Spinner } from "react-bootstrap";
+import grocery from "../images/grocery-logo.jpg";
+
+const LoginPage = ({ loading, error, ...props }) => {
+  const [values, setValues] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.authenticate();
+
+    userLogin(values)
+      .then((response) => {
+        console.log("response", response);
+        if (response.status === 200) {
+          const user = document.getElementById("username").value;
+
+          props.setUser(response.data);
+          props.history.push("/items");
+        } else {
+          props.loginFailure("Something Wrong!Please Try Again");
+        }
+      })
+      .catch((err) => {
+        if (err && err.response) {
+          switch (err.response.status) {
+            case 401:
+              console.log("401 status");
+              props.loginFailure("Authentication Failed.Bad Credentials");
+              break;
+            default:
+              props.loginFailure("Something Wrong!Please Try Again");
+          }
+        } else {
+          props.loginFailure("Something Wrong!Please Try Again");
+        }
+      });
+    //console.log("Loading again",loading);
+  };
+
+  const handleChange = (e) => {
+    e.persist();
+    setValues((values) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  console.log("Loading ", loading);
+  function signup() {
+    props.history.push("/signup");
+  }
+  return (
+    <div>
+      <div className="wrapper">
+        <form className="login" onSubmit={handleSubmit} noValidate={false}>
+          <img src={grocery} className="logo-grocery-login" alt=""></img>
+          <p className="title">Login</p>
+
+          <div className="form-group">
+            <input
+              id="username"
+              type="text"
+              className="form-control"
+              placeholder="username"
+              value={values.userName}
+              onChange={handleChange}
+              name="userName"
+              required
+              autofocus
+            />
+            <i className="fa fa-user" />
+            <div className="invalid-feedback">UserId is invalid</div>
+          </div>
+
+          <div className="form-group">
+            <input
+              id="password"
+              type="password"
+              className="form-control"
+              placeholder="password"
+              value={values.password}
+              onChange={handleChange}
+              name="password"
+              required
+            />
+            <i className="fa fa-key" />
+            <div className="invalid-feedback">Password is required</div>
+          </div>
+
+          <div className="form-group"></div>
+
+          <div className="form-group m-0">
+            <br></br>
+            <button type="submit" className="button-login">
+              Login
+              {loading && (
+                <Spinner
+                  as="span"
+                  animation="border"
+                  size="sm"
+                  role="status"
+                  aria-hidden="true"
+                />
+              )}
+            </button>
+            <a className="signup" onClick={signup}>
+              Not a user? Sign up
+            </a>
+          </div>
+          {error && (
+            <Alert style={{ marginTop: "20px" }} variant="danger">
+              {error}
+            </Alert>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = ({ auth }) => {
+  console.log("state ", auth);
+  return {
+    loading: auth.loading,
+    error: auth.error,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    authenticate: () => dispatch(authenticate()),
+    setUser: (data) => dispatch(authSuccess(data)),
+    loginFailure: (message) => dispatch(authFailure(message)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
